b2123b62de8e4f1c53814ebf8b13cdc9392af78a
maalison <151207237+maalison@users.noreply.github.com>
2023-12-01 19:27:07 -0600

    Update AoC2023D1.py

--- AoC2023D1.py
+++ AoC2023D1.py
@@ -4,7 +4,7 @@ with open('input.txt', 'r') as my_file:
 input = []
 nums = []
 num = ''
-numbers = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+
 
 
 sum = 0
